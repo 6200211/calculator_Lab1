8d869610ec4fffeeb9f83483594b7a4b43d828c3
ArtemPresniakov <6200211@gmail.com>
2024-09-28 16:28:22 +0300

    Added new text

--- calculator.cpp
+++ calculator.cpp
@@ -14,3 +14,15 @@ int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+//Hello, this is the first labwork 
+//Created by Artem Presniakov
+//Group number IO-21
+//Date 28.09.2024
+//Deadline date 29.09.2024
+//Year:2024
+//KPI 
+//FICE
+//3 year of education
+//bachelor qualification
+//3 years have gone of my first labwork)
+
